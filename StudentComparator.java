f7cc7e199ff1ebe10e36ad655a7e2107909e1562
Natalya <159589443+Natalya-Prokofeva@users.noreply.github.com>
2024-09-21 18:29:05 +0300

    Update StudentComparator.java

--- StudentComparator.java
+++ StudentComparator.java
@@ -1,12 +1,11 @@
 import java.util.Comparator;
 
-public class StudentComparator implements Comparator<Student>{
+public class StudentComparator implements Comparator<Student> {
 
     @Override
     public int compare(Student arg0, Student arg1) {
-        String name0 = arg0.getSurname() + arg0.getFirstname() + arg0.getPatronymic();
-        String name1 = arg1.getSurname() + arg1.getFirstname() + arg1.getPatronymic();
-        return name0.compareToIgnoreCase(name1);        
+        String fullName0 = arg0.getSurname() + arg0.getFirstname() + arg0.getPatronymic();
+        String fullName1 = arg1.getSurname() + arg1.getFirstname() + arg1.getPatronymic();
+        return fullName0.compareToIgnoreCase(fullName1);
     }
-    
 }
